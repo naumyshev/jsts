86437ae76f4caef4e3bcb5e0df1d631ff88c16c3
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-01-27 19:22:57 +0300

    Initial commit

---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,12 @@
+//export const value01_01 = '01_01'
+
+const sentence = 'Hello my friends'
+
+export const sum = (a: number, b:number) => a + b
+
+export const mult = (a: number, b:number) => a * b
+
+
+function splitIntoWords(sentence: string) {
+    return ['hello', 'my', 'friends']
+}
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+//import {value01_01} from "./01-hello-tests/01";
+
+//let a = value01_01
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
